9c5aef90c7801b0e20bcd8693fecba1258802b31
Knarkapan <johanroland@live.se>
2015-03-14 16:22:51 +0100

    derp

--- Raytracer.hs
+++ Raytracer.hs
@@ -39,121 +39,21 @@ createCamera (cdir1,cdir2,cdir3) (cpoint1,cpoint2,cpoint3) (cup1,cup2,cup3) fovI
                                                                                }
                 
                 
-dummyCam :: Camera
-dummyCam = createCamera (1,0,0) (0,0,0) (0,1,0) (45)
-               
- 
-dummyRay :: Ray
-dummyRay = Ray { dir =  R.fromListUnboxed (R.ix1 3) [5,0,0] 
-                ,point = R.fromListUnboxed (R.ix1 3) [0,0,0]
-               }
-               
-dummySphere :: Shape
-dummySphere = Sphere {
-                spos =  R.fromListUnboxed (R.ix1 3) [10,0,0] 
-                ,radius = 2.0
-            }
-            
-dummySphere2 :: Shape
-dummySphere2 = Sphere {
-                spos =  R.fromListUnboxed (R.ix1 3) [5,-3,-3] 
-                ,radius = 2.0
-            }
 
-dummyPlane :: Shape
-dummyPlane = Plane {
-                ppos =  R.fromListUnboxed (R.ix1 3) [0,-1,0] 
-                ,pnormal = R.fromListUnboxed (R.ix1 3) [0,1,0]
-            }
-
-dummyPlane2 :: Shape
-dummyPlane2 = Plane {
-                ppos =  R.fromListUnboxed (R.ix1 3) [0,0,1] 
-                ,pnormal = R.fromListUnboxed (R.ix1 3) [0,0,1]
-            }
-
-dummyPlane3 :: Shape
-dummyPlane3 = Plane {
-                ppos =  R.fromListUnboxed (R.ix1 3) [4,0,0] 
-                ,pnormal = R.fromListUnboxed (R.ix1 3) [1,0,0]
-            }
-            
-dummyObj = Object{shape =dummySphere
-             , color=(0,255,0) 
-             ,reflectance = 0
-             ,shininess=16
-             }
-           
-            
-dummyWorld :: World
-dummyWorld = World{items = [Object{shape =dummySphere
-             , color=(0,0,255)  
-             ,reflectance = 0
-             ,shininess=16},
-             Object{shape =dummyPlane
-             , color= (150,0,0)  
-             ,reflectance = 1.0
-             ,shininess= 16}]
-             ,lights = [Light{ 
-                lpos =  R.fromListUnboxed (R.ix1 3) [20,10,0]
-                ,lcolor = (255,255,255)
-             },Light{ 
-                lpos =  R.fromListUnboxed (R.ix1 3) [5,7,0]
-                ,lcolor = (255,255,255)
-             }]
-             }
-
-dummyWorld2 :: World 
-dummyWorld2 = addLightToWorld (createLight (6.0,0.0,0.0) (t2c White))
-    --(addLightToWorld (createLight (4.0,0.0,0.0) (t2c White))
-    --(addLightToWorld (createLight (2.0,0,2.0) (t2c White))
-    (addLightToWorld (createLight (2.0,0,-2.0) (t2c White))
-    (addObjectToWorld (createPlane vUp vDown (0.0,150.0,0.0) 1 16)
-    (addObjectToWorld (createSphere 2.0 (10.0,0.0,0.0) (t2c Blue) 0 16)
-    (addObjectToWorld (createPlane vDown vUp (150.0,0.0,0.0) 1 16) emptyWorld )))) --))
-     
-    
-
-dummyVec1 :: DoubleVector
-dummyVec1 = R.fromListUnboxed (R.ix1 3) [1,0,0]
-
-
-dummyVec2 :: DoubleVector
-dummyVec2 = R.fromListUnboxed (R.ix1 4) [20,1,0,0]
-
-cameraRay :: Camera->(Int, Int) ->Int -> Int -> Ray
-cameraRay r@Camera{cdir = dir, cpoint = pnt, cup =u} (maxX',maxY') x y =
-     Ray{dir= ( R.computeUnboxedS( R.zipWith (-) imagePoint pnt)), point = pnt}
-        where cam_right = crossProd dir u
-              maxX = (fromIntegral maxX' )
-              maxY = (fromIntegral maxY' )
-              normX = ((fromIntegral x ) /maxX) -0.5
-              normY =  ((fromIntegral y ) /maxY) -0.5
-              imagePoint = (R.zipWith (+) (R.zipWith (+) (R.zipWith (+) (R.map (normX*) u)
-                    (R.map (normY*) cam_right)) pnt) dir )
-
-cameraRay2 :: Camera->(Int, Int) -> Int -> Int -> Ray
-cameraRay2 r@Camera{cdir = dir, cpoint = pnt, cup =up, fov= fov'} (maxX',maxY') x y =
+cameraRay :: Camera->(Int, Int) -> Int -> Int -> Ray
+cameraRay r@Camera{cdir = dir, cpoint = pnt, cup =up, fov= fov'} (maxX',maxY') x y =
      Ray{dir= normalize( R.computeUnboxedS(R.zipWith (+) pnt imagePoint)), point = pnt}  
             where u = crossProd dir up
                   v = crossProd u dir
                   halfWidth = 1.0--200
                   halfHeight = 1.0 --200
                   viewPlaneHalfWith = tan fov' --(pi/4.0)
-                  aspectRatio = 1
+                  aspectRatio = (fromIntegral maxX')/(fromIntegral maxY')
                   viewPlaneHalfHeight = viewPlaneHalfWith * aspectRatio
                   viewPlaneBottomLeftPoint = R.zipWith (-) ( R.zipWith (-) (dir) (R.map ( viewPlaneHalfHeight* ) v))  (R.map ( viewPlaneHalfWith* ) u)
                   xIncVector = R.map (*(2*halfWidth/ (fromIntegral maxX'))) v
                   yIncVector = R.map (*(2*halfHeight/ (fromIntegral maxY'))) u
                   imagePoint = R.zipWith (+) (R.zipWith (+) viewPlaneBottomLeftPoint (R.map ( (fromIntegral x)* ) xIncVector)) (R.map ( ( fromIntegral y)* ) yIncVector)
-
-cameraRay3 :: Camera -> (Int, Int) -> Int -> Int -> Ray
-cameraRay3 r@Camera{cdir = dir, cpoint = pnt, cup =up} (maxX,maxY) x y =
-     Ray{dir= (R.computeUnboxedS pixelDir), point = pnt}  
-            where cr = crossProd dir up
-                  cu = crossProd cr dir
-                  dist =(0.5 / (tan (90.0/2.0)))
-                  pixelDir = (R.zipWith (+) (R.zipWith (+) (R.map (dist*) dir) (R.map ((0.5 - (fromIntegral y)/( fromIntegral(maxY -1)))*) cr) ) (R.map ((0.5 - (fromIntegral x)/( fromIntegral(maxX -1)))*) cu))
                   
    
 trace2Array::[Entity]-> Camera ->(Int,Int)-> Int -> R.Array R.U R.DIM2 Color
@@ -179,7 +79,7 @@ avrageCol::Color -> Color-> Color
 avrageCol (a,b,c) (a1,b1,c1) = ((a+a1)/2.0,(b+b1)/2.0,(c+c1)/2.0)
 
 traceFunc :: (R.DIM2 -> Color) -> R.DIM2 -> Int -> Int -> World -> Camera -> Int-> Color --(Word8
-traceFunc _ (R.Z R.:. ax R.:. ay) widht height w c bounses = unsafePerformIO(trace w (cameraRay2 (c) (widht,height) ax ay) 0 bounses)
+traceFunc _ (R.Z R.:. ax R.:. ay) widht height w c bounses = unsafePerformIO(trace w (cameraRay (c) (widht,height) ax ay) 0 bounses)
 
 
 trace :: World -> Ray -> Depth -> Int-> IO Color
