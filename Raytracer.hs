b19b5ce5e3d8dfb7de411808cbf8b0449c8c1153
Knarkapan <johanroland@live.se>
2015-03-14 11:07:12 +0100

    push

--- Raytracer.hs
+++ Raytracer.hs
@@ -154,44 +154,44 @@ main :: IO ()
 main = do
     putStrLn $ "Starting trace on a " ++ show 100 ++ "x" ++ show 100 ++ " ..."
     t0 <- getCurrentTime
-    (trace2BMP obj dummyCam (100,100) "test.bmp")
+    (trace2BMP obj dummyCam (100,100) 2 "test.bmp")
     t1 <- getCurrentTime
     putStrLn $ "Trace is done (in "++ show (diffUTCTime t1 t0) ++") creating image named " ++ "test.bmp"
     where
        obj = [EntO (createPlane vUp vDown (0.0,150.0,0.0) 1),EntO (createSphere 2.0 (10.0,0.0,0.0) (t2c Blue) 0 ),EntL (createLight (6.0,0.0,0.0) (t2c White))]
     
-trace2Array::[Entity]-> Camera ->(Int,Int)-> R.Array R.U R.DIM2 Color
-trace2Array ent camera (widht,height) = finalasDouble
+trace2Array::[Entity]-> Camera ->(Int,Int)-> Int -> R.Array R.U R.DIM2 Color
+trace2Array ent camera (widht,height) bounses = finalasDouble
     where
      ents = createWorld ent
      ents' = execState ents emptyWorld
      indexs = [(0,0,0)| x<- [0..(widht-1)], y <- [0..(height-1)] ]
      image = R.fromListUnboxed (R.ix2 widht height) indexs
-     finalasDouble = R.computeUnboxedS $ R.traverse image id (\f  x ->  multPixtraceFunc  f x widht height ents' camera ) 
+     finalasDouble = R.computeUnboxedS $ R.traverse image id (\f  x ->  multPixtraceFunc  f x widht height ents' camera bounses ) 
 
     
-trace2BMP::[Entity]-> Camera ->(Int,Int)-> String -> IO()
-trace2BMP ent camera (widht,height) fName = do
-    let image = trace2Array ent camera (widht,height)
+trace2BMP::[Entity]-> Camera ->(Int,Int)-> Int -> String -> IO()
+trace2BMP ent camera (widht,height) bounses fName  = do
+    let image = trace2Array ent camera (widht,height) bounses
     let final = R.computeUnboxedS $ R.map convertColtoFCol image
     writeImageToBMP ("./"++fName) final
 
-multPixtraceFunc::(R.DIM2 -> Color) -> R.DIM2 -> Int -> Int -> World -> Camera -> Color
-multPixtraceFunc f (R.Z R.:. ax R.:. ay) widht height w c =  foldr1 (avrageCol) [traceFunc f (R.Z R.:. ax R.:. ay) widht height w c | _<-[1..5]]
+multPixtraceFunc::(R.DIM2 -> Color) -> R.DIM2 -> Int -> Int -> World -> Camera-> Int -> Color
+multPixtraceFunc f (R.Z R.:. ax R.:. ay) widht height w c  bounses=  foldr1 (avrageCol) [traceFunc f (R.Z R.:. ax R.:. ay) widht height w c bounses | _<-[1..5]]
 
 avrageCol::Color -> Color-> Color
 avrageCol (a,b,c) (a1,b1,c1) = ((a+a1)/2.0,(b+b1)/2.0,(c+c1)/2.0)
 
-traceFunc :: (R.DIM2 -> Color) -> R.DIM2 -> Int -> Int -> World -> Camera -> Color --(Word8
-traceFunc _ (R.Z R.:. ax R.:. ay) widht height w c = unsafePerformIO(trace w (cameraRay2 (c) (widht,height) ax ay) 0)
+traceFunc :: (R.DIM2 -> Color) -> R.DIM2 -> Int -> Int -> World -> Camera -> Int-> Color --(Word8
+traceFunc _ (R.Z R.:. ax R.:. ay) widht height w c bounses = unsafePerformIO(trace w (cameraRay2 (c) (widht,height) ax ay) 0 bounses)
 
 
-trace :: World -> Ray -> Depth -> IO Color
-trace w r@Ray{dir = dir', point = pnt} d = do
-    case d of
-        2 ->do
+trace :: World -> Ray -> Depth -> Int-> IO Color
+trace w r@Ray{dir = dir', point = pnt} d bounses = do
+    case d==bounses of
+        True ->do
             return $ (0,0,0)     -- byt 5an till dynamisk?
-        _ -> do 
+        False -> do 
             test <-intersectWorld r w 
             case test of
                 Nothing -> return $ (0,0,0)
@@ -204,7 +204,7 @@ trace w r@Ray{dir = dir', point = pnt} d = do
                     let cos_theta =abs cos_theta' 
                     let brdf' = 2 * (reflectance obj) -- * cos_theta
                     let brdf = minimum [brdf', 1/pi]
-                    reflCol <- trace w (Ray{dir=newDir, point = hitp}) (d+1) 
+                    reflCol <- trace w (Ray{dir=newDir, point = hitp}) (d+1) bounses 
                     calcFinalCol emittance reflCol brdf lightIntens
                     
                     
